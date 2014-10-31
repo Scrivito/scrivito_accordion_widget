85621ff0ea4b9c155f232ef3e960ec90f67f16fc
Karina <kt@jetaido.com>
2014-10-31 18:09:42 +0100

    changes for new SDK (scrivito-0.30.0.rc1), some minor tweaks

--- app/models/accordion_panel_widget.rb
+++ app/models/accordion_panel_widget.rb
@@ -1,5 +1,11 @@
 class AccordionPanelWidget < Widget
+
   def active_class
     "in" if self.container.panels.first == self
   end
+
+  def description_for_editor
+    "Accordion Panel"
+  end
+
 end
--- app/models/accordion_widget.rb
+++ app/models/accordion_widget.rb
@@ -1,5 +1,11 @@
 class AccordionWidget < Widget
+
+  def description_for_editor
+    "Accordion"
+  end
+
   def valid_widget_classes_for(field_name)
     %w[AccordionPanelWidget]
   end
+
 end
