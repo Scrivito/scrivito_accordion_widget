48ed4b193695c864bfe3bb95b31698c9305bc753
Gert Geidel <gert.geidel@infopark.de>
2015-09-14 10:47:04 +0200

    Remove dead code

--- app/assets/javascripts/scrivito_accordion_widget.js
+++ app/assets/javascripts/scrivito_accordion_widget.js
@@ -9,10 +9,8 @@
           var accordion_title = $(accordion_title);
           accordion_title.click(function() {
             accordion_group.find('.scrivito-accordion-title').removeClass('scrivito-accordion-active');
-            //accordion_group.find('.scrivito-accordion-content').stop(true,false).slideUp(350);
 
             accordion_title.addClass('scrivito-accordion-active');
-            //accordion_title.next().stop(true,false).slideDown(350);
           });
         });
       });
