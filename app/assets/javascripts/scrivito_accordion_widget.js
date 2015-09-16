dc03ec44772a1e6456c4878a200eebb0948cc85b
Gert Geidel <gert.geidel@infopark.de>
2015-09-16 14:18:17 +0200

    Fix for scrivito on content

--- app/assets/javascripts/scrivito_accordion_widget.js
+++ app/assets/javascripts/scrivito_accordion_widget.js
@@ -2,19 +2,17 @@
   'use strict';
 
   $(function() {
-    scrivito.on('content', function(content) {
-      $(content).find('.scrivito-accordion-group').each(function(i, group) {
-        var accordion_group = $(group);
-        accordion_group.find('.scrivito-accordion-title').each(function(i, accordion_title) {
-          var accordion_title = $(accordion_title);
-          accordion_title.click(function() {
-            accordion_group.find('.scrivito-accordion-title').removeClass('scrivito-accordion-active');
-            accordion_group.find('.scrivito-accordion-content').stop(true,false).slideUp(350);
+    $('.scrivito-accordion-group').each(function(i, group) {
+      var accordion_group = $(group);
+      accordion_group.find('.scrivito-accordion-title').each(function(i, accordion_title) {
+        var accordion_title = $(accordion_title);
+        accordion_title.on('click', function() {
+          accordion_group.find('.scrivito-accordion-title').removeClass('scrivito-accordion-active');
+          accordion_group.find('.scrivito-accordion-content').stop(true,false).slideUp(350);
 
 
-            accordion_title.addClass('scrivito-accordion-active');
-            accordion_title.next().stop(true,false).slideDown(350);
-          });
+          accordion_title.addClass('scrivito-accordion-active');
+          accordion_title.next().stop(true,false).slideDown(350);
         });
       });
     });
